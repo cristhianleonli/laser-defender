a1f8b335a295cc218f568081d0fb28e6c3b5031c
Cristhian Leon <cris.leonardoleon@gmail.com>
2020-12-06 18:15:48 +0100

    - Add Enemy prefab - Add Path prefab - Add EnemyPathing to move the enemy across the screen - Create a wave config asset file to keep the data of the wave of enemies

---
+++ Assets/Scripts/Enemies/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] WaveConfig waveConfig;
+    private List<Transform> waypoints;
+    private float moveSpeed => waveConfig.MoveSpeed;
+
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(
+                transform.position, targetPosition, movementFrame
+            );
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/WaveConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy/Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] public GameObject EnemyPrefab;
+    [SerializeField] public GameObject PathPrefab;
+    [SerializeField] public float TimeBetweenSpawns = 0.5f;
+    [SerializeField] public float SpawnRandomFactor = 0.3f;
+    [SerializeField] public int NumberOfEnemies = 5;
+    [SerializeField] public float MoveSpeed = 2f;
+
+    public List<Transform> GetWaypoints()
+    {
+        var waypoints = new List<Transform>();
+
+        foreach (Transform child in PathPrefab.transform)
+        {
+            waypoints.Add(child);
+        }
+
+        return waypoints;
+    }
+}
