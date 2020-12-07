7174bcd4ce2e9537f6cf51e7d5e7c80cc7f090f0
Cristhian Leon <cris.leonardoleon@gmail.com>
2020-12-07 21:56:06 +0100

    damage dealer added

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float healt = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 3f;
+
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    void Update()
+    {
+        CountDownShoot();
+    }
+
+    private void CountDownShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0)
+        {
+            Fire();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            projectile,
+            transform.position,
+            Quaternion.identity
+        );
+
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        healt -= damageDealer.GetDamage();
+
+        if (healt <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
