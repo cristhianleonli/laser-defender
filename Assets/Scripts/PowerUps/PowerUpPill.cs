14fbca23b10d0a18489ad91a7d016cbbfdf449aa
Cristhian Leon <cris.leonardoleon@gmail.com>
2020-12-10 21:34:29 +0100

    added pill class

--- Assets/Scripts/PowerUps/PowerUpPill.cs
+++ Assets/Scripts/PowerUps/PowerUpPill.cs
@@ -1,8 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+enum PillType
+{
+    Blue,
+    Yellow
+}
 
 public class PowerUpPill : PowerUp
 {
-    
+    [SerializeField] private PillType pillType;
+
+    private void Start()
+    {
+        string resourceName = pillType == PillType.Blue ? "pill_blue" : "pill_yellow";
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Assets/Sprites/PowerUps/" + resourceName);
+    }
 }
